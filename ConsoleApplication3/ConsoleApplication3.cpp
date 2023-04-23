b33d313ef1043536bdc738ec3a1eed7f26862d77
nikit <nikit@DESKTOP-5363RTP>
2023-04-23 15:45:29 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,23 @@
+﻿
+#include<iostream>
+#include<fstream>
+class Road {
+public:
+	double length;
+	int width;
+};
+
+
+int main() {
+	using namespace std;
+	Road road;
+	std::ifstream fIn;
+	fIn.open("C:\visual\kasatka\2\ConsoleApplication3\ConsoleApplication3\\length.txt");
+	fIn >> road.length;
+	fIn.close();
+	fIn.open("C:\visual\kasatka\2\ConsoleApplication3\ConsoleApplication3\\width.txt");
+	fIn >> road.width;
+	fIn.close();
+	std::cout << "Lenght: " << road.length << endl;
+	std::cout << "Width: " << road.width << endl;
+};
